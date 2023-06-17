3adbd0f1899f7f21ae1b1b5e5c926e8c64d0450b
mary446 <135317707+mary446@users.noreply.github.com>
2023-06-17 14:09:08 +0300

    tried the assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+li.textContent = "Hi there!";
+// => <li>Hi there!</li>
+console.log(li.textContent);
+// => "Hi there!"
+const main = document.getElementById("main");
+main.innerHTML =
+"<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
